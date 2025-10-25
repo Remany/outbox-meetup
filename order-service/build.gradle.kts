1782604e49bb64a88df8cc173d06a535ca72a299
Leonid Romanov <romanovleonid82@gmail.com>
2025-10-25 20:50:58 +0300

    Настроен observability стек

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -1,12 +1,27 @@
 plugins {
+    idea
     java
+    id("maven-publish")
     id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "ru.romanov"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 description = "order-service"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "nexus"
+        url = uri(System.getenv("NEXUS_URL") ?: System.getProperty("NEXUS_URL") ?: "http://localhost:8081/repository/maven-releases")
+        isAllowInsecureProtocol = true
+        credentials {
+            username = System.getenv("NEXUS_USERNAME") ?: System.getProperty("NEXUS_USERNAME") ?: "admin"
+            password = System.getenv("NEXUS_PASSWORD") ?: System.getProperty("NEXUS_PASSWORD") ?: "admin"
+        }
+    }
+}
 
 configurations {
     compileOnly {
@@ -20,24 +35,112 @@ java {
     }
 }
 
-repositories {
-    mavenLocal()
-    mavenCentral()
+val versions = mapOf(
+    "logbackVersion" to "1.5.18",
+    "outboxLibVersion" to "1.0.0",
+    "opentelemetryBomVersion" to "2.15.0"
+)
+
+dependencyManagement {
+    imports {
+        mavenBom("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:${versions["opentelemetryBomVersion"]}")
+    }
 }
 
 dependencies {
+    // OUTBOX-LIB
+    implementation("ru.romanov:outbox-processor:${versions["outboxLibVersion"]}")
+
+    // SPRING
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("ru.romanov:outbox-processor:1.0.0")
+
+    // OBSERVABILITY
+    implementation("io.micrometer:micrometer-registry-prometheus")
+    implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+    implementation("io.micrometer:micrometer-observation")
+    implementation("io.micrometer:micrometer-tracing")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter")
+    implementation("ch.qos.logback:logback-classic:${versions["logbackVersion"]}")
+
+    // DB
     implementation("org.liquibase:liquibase-core")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
+
     annotationProcessor("org.projectlombok:lombok")
+
+    // TEST
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<AbstractPublishToMaven> {
+    dependsOn(tasks.named("jar"))
+}
+
+/*
+──────────────────────────────────────────────────────
+============== Resolve NEXUS credentials ==============
+──────────────────────────────────────────────────────
+*/
+
+file(".env").takeIf { it.exists() }?.readLines()?.forEach {
+    val (k, v) = it.split("=", limit = 2)
+    System.setProperty(k.trim(), v.trim())
+    logger.lifecycle("${k.trim()}=${v.trim()}")
+}
+
+val nexusUrl = System.getenv("NEXUS_URL") ?: System.getProperty("NEXUS_URL")
+val nexusUser = System.getenv("NEXUS_USERNAME") ?: System.getProperty("NEXUS_USERNAME")
+val nexusPassword = System.getenv("NEXUS_PASSWORD") ?: System.getProperty("NEXUS_PASSWORD")
+
+if (nexusUrl.isNullOrBlank() || nexusUser.isNullOrBlank() || nexusPassword.isNullOrBlank()) {
+    throw GradleException(
+        "NEXUS details are not set. Create a .env file with correct properties: " + "NEXUS_URL, NEXUS_USERNAME, NEXUS_PASSWORD"
+    )
+}
+
+/*
+──────────────────────────────────────────────────────
+============== Nexus Publishing ==============
+──────────────────────────────────────────────────────
+*/
+
+publishing {
+    publications {
+        val jarBaseName = name
+        val jarFile = file("build/libs").listFiles()
+            ?.firstOrNull { it.name.contains(name) && (it.extension == "jar" || it.extension == "zip") }
+
+        if (jarFile != null) {
+            logger.lifecycle("publishing: ${jarFile.name}")
+
+            create<MavenPublication>("publish${name.replaceFirstChar(Char::uppercase)}Jar") {
+                artifact(jarFile)
+                groupId = "ru.romanov"
+                artifactId = jarBaseName
+                version = "1.0.0"
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "nexus"
+            url = uri(nexusUrl)
+            isAllowInsecureProtocol = true
+            credentials {
+                username = nexusUser
+                password = nexusPassword
+            }
+        }
+    }
 }
